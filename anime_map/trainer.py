a5cdf8e276be41f9005e1e1e324a864c64349ecc
Tanguy-lm <73ubgB3daxtnYwp>
2021-08-27 17:55:29 +0200

    update 27_08

--- anime_map/trainer.py
+++ anime_map/trainer.py
@@ -1,15 +1,15 @@
 from anime_map.data import process_data
 from anime_map.pipeline import model_knn_anime_map
 
-name_file = 
+name_file = 'active_users_df_10PlusRatings_partial'
 
 class Trainer:
-    def __init__(self,pca_pivot)
+    def __init__(self, pca_pivot):
         self.pca_pivot = pca_pivot
         
-    def tain(self):
-        model_knn_anime_map(pca_pivot)
+    def train(self):
+        model_knn_anime_map(self.pca_pivot)
 
 if __name__ == '__main__':
-    pivot_df, anime_name_pivot_df = process_data('animelist')
+    pivot_df, anime_name_pivot_df = process_data(name_file)
     
