593cefcd79a59cd60e90a983eab865526dcbe504
Tanguy-lm <73ubgB3daxtnYwp>
2021-08-29 14:09:53 +0200

    save dataset on google for rating_complete_10plus_PG.csv

--- anime_map/data.py
+++ anime_map/data.py
@@ -3,7 +3,7 @@ import joblib
 from google.cloud import storage
 
 name_file = "rating_complete"
-minimun_of_rating = 10
+minimun_of_rating = 100
 new_df = f'{name_file}_{minimun_of_rating}plus_PG'
 BUCKET_NAME = 'wagon-data-664-le_mehaute'
 STORAGE_LOCATION = f'anime_map_data/{new_df}.csv'
