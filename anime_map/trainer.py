3e73b23513903b1d4c9d63cbb1695bb36717f918
Tanguy-lm <73ubgB3daxtnYwp>
2021-08-30 22:14:08 +0200

    last update 30_08

--- anime_map/trainer.py
+++ anime_map/trainer.py
@@ -1,5 +1,6 @@
 from google.cloud import storage
 from anime_map.pipeline import model_knn_anime_map, PCA_vector
+from anime_map.data import get_anime
 import numpy as np
 import joblib
 import pandas as pd
@@ -25,19 +26,32 @@ class Trainer:
         return model_knn_anime_map(self.pca_pivot)
 
 def pivot_matrix(df):
+    
+    #all anime_id on df
+    anime_id_df =  df.sort_values(by=['anime_id']).drop_duplicates('anime_id').reset_index()[['anime_id']]
+    # genres by anime_id
+    anime_genre_df = get_anime()[['anime_id','Genres']]
+    #just gor anime_id in this df
+    anime_genre_df = anime_genre_df.merge(anime_id_df, on='anime_id',how='inner')
+    #one hot encod genres for anime_genre_df
+    anime_genres_df_encoded = pd.concat(objs = [anime_genre_df.drop(columns = 'Genres', axis =1), name['Genres'].str.get_dummies(sep=", ")], axis = 1)
+    anime_genres_df_encoded = anime_genres_df_encoded.set_index('anime_id')
+    #convert to numpy array
+    anime_genres_np = anime_genres_df_encoded.to_numpy()
+    
+    #convert df to numpy array
     np_df = df.to_numpy()
-    print('starting pivot with numpy')
+    # vectorize np_df to pivot_table
     cols, col_pos = np.unique(np_df[:, 0], return_inverse=True)
     rows, row_pos = np.unique(np_df[:, 1], return_inverse=True)
-
-    print('create cols and rows')
     
     pivot_table = np.zeros((len(rows), len(cols)), dtype=np_df.dtype)
-    print('pivot_table full of zeros')
     pivot_table[row_pos, col_pos] = np_df[:, 2]
-    print('pivot_table completed')
     
-    return pivot_table
+    #join the array to a complet pivot_table
+    pivot_table = np.concatenate((pivot_table, anime_genres_np), axis=1)
+    
+    return pivot_table,anime_id_df
 
 def upload_model_to_gcp():
 
@@ -67,7 +81,7 @@ def save_model(reg):
 def save_vector(reg,new_df):
     """method that saves the model into a .joblib file and uploads it on Google Storage /models folder
     HINTS : use joblib library and google-cloud-storage"""
-    STORAGE_LOCATION = f'anime_map_data/{new_df}.csv'
+    STORAGE_LOCATION_ = f'anime_map_data/{new_df}.csv'
     # saving the trained model to disk is mandatory to then beeing able to upload it to storage
     # Implement here
     #joblib.dump(reg, f'{new_df}.joblib')
@@ -75,16 +89,25 @@ def save_vector(reg,new_df):
     print(f"saved {new_df}.csv locally")
 
     # Implement here
-    upload_model_to_gcp(new_df)
-    print(f"uploaded {new_df}.csv to gcp cloud storage under \n => {STORAGE_LOCATION}")
+    client = storage.Client()
+
+    bucket = client.bucket(BUCKET_NAME)
+
+    blob = bucket.blob(STORAGE_LOCATION_)
+
+    blob.upload_from_filename(f'{new_df}.csv')
+    print(f"uploaded {new_df}.csv to gcp cloud storage under \n => {STORAGE_LOCATION_}")
 
 if __name__ == '__main__':
     df = get_data()
-    df = pivot_matrix(df)
+    print(f'anime_name_df shape {df.shape}')
+    df, anime_id_df = pivot_matrix(df)
+    save_vector(anime_id_df,f'{name_file}_anime_id_df')
     print('pivot_matrix step ok')
+    print(f'pivot_matrix shape {df.shape}')
     print(df)
     df =  PCA_vector(df)
-    save_vector(df,f'{name_file}_pivot_df')
+    save_vector(df,f'{name_file}_pivot_pca_df')
     model = Trainer(df)
     print('Trainer step ok')
     model_knn = model.train()
